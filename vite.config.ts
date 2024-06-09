0e11169e44a11c25ca02b50a405b5d810ca6670a
German <germancanteros26@gmail.com>
2024-06-08 23:40:40 -0300

    Cambios de produccion 11

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react-swc'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base:'/ClaraPH/'
+  base:'https://gerdev26.github.io/ClaraPH'
 })
