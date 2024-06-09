6f70b449f5fa2f0cc6351fd6e764ed16374d2e5d
German <germancanteros26@gmail.com>
2024-06-09 00:44:41 -0300

    Se desintalo taos 2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,7 @@
-// tailwind.config.js
 module.exports = {
-  content: {
-    relative: true,
-    transform: (content) => content.replace(/taos:/g, ''),
-    files: ['./src/**/*.{html,js,jsx,ts,tsx}']
-  },
+  content: ['./src/**/*.{html,js,jsx,ts,tsx}'],
   theme: {
     extend: {}
   },
-  plugins: [
-    require('taos/plugin')
-  ],
-  safelist: [
-    '!duration-[0ms]',
-    '!delay-[0ms]',
-    'html.js :where([class*="taos:"]:not(.taos-init))'
-  ]
-}
+  plugins: []
+};
